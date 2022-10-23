b56d9b4dadcccd9e59af0beb453c16b7167a1e51
Eduardo Lemus <edventura91@gmail.com>
2022-10-22 21:14:01 -0700

    Updated client to take into account clientid

--- run_client.py
+++ run_client.py
@@ -4,10 +4,22 @@ from multiprocessing import Process
 
 def client():
 
-	command = 'sh runClient.sh'
+	f = open("runClient"+str(m)+".sh", "w")
+	f.write("#!/bin/bash\n")
+	f.write('export SVR_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"\n')
+	f.write('echo "** starting client from ${SVR_HOME} **"\n')
+	f.write("JAVA_MAIN=' gash.grpc.route.client.RouteClient'\n")
+	f.write('JAVA_ARGS="request"\n')
+	f.write('JAVA_ARGs2="'+str(m)+'"\n')
+	f.write("JAVA_TUNE='-client -Xms96m -Xmx512m'\n")
+	f.write("java ${JAVA_TUNE} -cp .:${SVR_HOME}/lib/'*':${SVR_HOME}/bin ${JAVA_MAIN} ${JAVA_ARGS} ${JAVA_ARGs2}\n")
+	f.close()
+	command = 'sh runClient'+str(m)+'.sh'
+	#command = 'java -client -Xms96m -Xmx512m -cp ./lib/*:./bin  gash.grpc.route.client.RouteClient request 1'
 	process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 	output, error = process.communicate()
 	print(output)
+
 
 if __name__ == "__main__":
 	print("here")
@@ -15,8 +27,8 @@ if __name__ == "__main__":
 	processes=[]
 	print(number_of_clients)
 	for m in range(1,number_of_clients):
-		n = m + 1
-		p = Process(target=client)
+		
+		p = Process(target=client, args=(m,))
 		p.start()
 		processes.append(p)
 		
