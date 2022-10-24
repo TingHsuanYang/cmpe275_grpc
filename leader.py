fa6d1ccef37573567ab270ff99e110034366a749
Eduardo Lemus <edventura91@gmail.com>
2022-10-24 11:02:21 -0700

    fix leader.py

--- leader.py
+++ leader.py
@@ -13,11 +13,62 @@ def is_port_in_use(port: int) -> bool:
         return s.connect_ex(('0.0.0.0',int(port))) == 0
 
 def check_Add():
-	for m in range(1,9):
-		if is_port_in_use(str(m)*4):
+	for m in range(0,9):
+		if is_port_in_use(str(111)+str(m)):
 			pass
 		else:
-			return m
+			return str(111)+str(m)
+			
+	for m in range(0,9):
+		if is_port_in_use(str(112)+str(m)):
+			pass
+		else:
+			return str(112)+str(m)
+	for m in range(0,9):
+		if is_port_in_use(str(113)+str(m)):
+			pass
+		else:
+			return str(113)+str(m)
+	for m in range(0,9):
+		if is_port_in_use(str(114)+str(m)):
+			pass
+		else:
+			return str(114)+str(m)
+	for m in range(0,9):
+		if is_port_in_use(str(115)+str(m)):
+			pass
+		else:
+			return str(115)+str(m)
+	for m in range(0,9):
+		if is_port_in_use(str(116)+str(m)):
+			pass
+		else:
+			return str(116)+str(m)
+	for m in range(0,9):
+		if is_port_in_use(str(117)+str(m)):
+			pass
+		else:
+			return str(117)+str(m)
+	for m in range(0,9):
+		if is_port_in_use(str(118)+str(m)):
+			pass
+		else:
+			return str(118)+str(m)
+	for m in range(0,9):
+		if is_port_in_use(str(119)+str(m)):
+			pass
+		else:
+			return str(119)+str(m)
+	for m in range(0,9):
+		if is_port_in_use(str(121)+str(m)):
+			pass
+		else:
+			return str(121)+str(m)
+	for m in range(0,9):
+		if is_port_in_use(str(123)+str(m)):
+			pass
+		else:
+			return str(123)+str(m)
 
 def check_Delete():
 	for m in range(9,0,-1):
@@ -36,15 +87,16 @@ def server(config_file):
 def add_server():
 	#print(len(servers))
 	sernum=check_Add()
-	next = sernum+1
+	next = int(sernum)+1
 	f = open("server"+str(sernum)+".txt", "w")
-	f.write("server.id="+str(sernum)*4+"\n")
-	f.write("server.port="+str(sernum)*4+"\n")
+	f.write("server.id="+str(sernum)+"\n")
+	f.write("server.port="+str(sernum)+"\n")
 	f.write("server.role=worker|monitor|leader"+"\n")
-	f.write("server.next.id="+str(next)*4+"\n")
-	f.write("server.next.port="+str(next)*4+"\n")
+	f.write("server.next.id="+str(next)+"\n")
+	f.write("server.next.port="+str(next)+"\n")
 	f.close()
 	server("server"+str(sernum)+".txt")
+		
 		
 
 def delete_server():
